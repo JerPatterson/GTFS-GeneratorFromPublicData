9be9adba1f43ad918bd64cd608c9988738ca428a
JerPatterson <jeremie.patterson@polymtl.ca>
2025-07-09 17:21:37 -0400

    fix issue with .pdf hosted on google drive

--- mrcjoliette/fetch.py
+++ mrcjoliette/fetch.py
@@ -1,12 +1,14 @@
+import os
+import gdown
 import urllib.request
 
-SCHEDULES_PATH = "schedules"
+SCHEDULES_PATH = "./schedules"
 
 ROUTE_32_SCHEDULE_URL = "https://mrcjoliette.qc.ca/wp-content/uploads/2024/01/horaire-32-horaire-20240115.pdf"
 ROUTE_34_SCHEDULE_URL = "https://mrcjoliette.qc.ca/wp-content/uploads/2024/01/horaire-34-2024-0115.pdf"
 ROUTE_50_SCHEDULE_URL = "https://mrcjoliette.qc.ca/wp-content/uploads/2024/08/horaire-50-horaire-28-avril-2024-vf.pdf"
 ROUTE_125_SCHEDULE_URL = "https://mrcjoliette.qc.ca/wp-content/uploads/2020/07/20200831-horaire-125-recto-verso-31-aout-2020.pdf"
-ROUTE_131_138_SCHEDULE_URL = "https://drive.google.com/file/d/1yz7RamN5h8BZQWTpdkymo9yDur1RscCA/view"
+ROUTE_131_138_SCHEDULE_URL = "https://drive.google.com/uc?id=1yz7RamN5h8BZQWTpdkymo9yDur1RscCA"
 
 
 def download_schedule_file(url, route_name):
@@ -16,12 +18,18 @@ def download_schedule_file(url, route_name):
         
     print(f"Downloaded schedule for route {route_name}.")
 
+def download_schedule_file_from_google_drive(url, route_name):
+    gdown.download(url, f"{SCHEDULES_PATH}/{route_name}.pdf", quiet=True)
+    print(f"Downloaded schedule for route {route_name}.")
+
 def download_regional_routes_schedules():
+    os.makedirs(SCHEDULES_PATH, exist_ok=True)
+
     download_schedule_file(ROUTE_32_SCHEDULE_URL, "32")
     download_schedule_file(ROUTE_34_SCHEDULE_URL, "34")
     download_schedule_file(ROUTE_50_SCHEDULE_URL, "50")
     download_schedule_file(ROUTE_125_SCHEDULE_URL, "125")
-    download_schedule_file(ROUTE_131_138_SCHEDULE_URL, "131_138")
+    download_schedule_file_from_google_drive(ROUTE_131_138_SCHEDULE_URL, "131_138")
 
 
 def main():
