1d042bcc7c205128f15dd9ea0cf0c1d7ba7c04f9
JerPatterson <jeremie.patterson@polymtl.ca>
2025-07-13 20:57:00 -0400

    add code for agency.txt file creation

---
+++ mrcjoliette/agency_builder.py
@@ -0,0 +1,26 @@
+import os
+
+GTFS_OUTPUT_PATH = "./output"
+
+KML_NAMESPACE = {
+    "kml": "http://www.opengis.net/kml/2.2"
+}
+
+
+def create_agency_content() -> None:
+    os.makedirs(GTFS_OUTPUT_PATH, exist_ok=True)
+
+    agency_txt_content = "agency_id,agency_name,agency_url,agency_timezone,agency_lang,agency_phone,agency_email\n"
+    agency_txt_content += "mrcjoliette,MRC de Joliette,https://mrcjoliette.qc.ca/transport-mrc-joliette/,America/Montreal,fr,450-756-2785,info.transport@mrcjoliette.qc.ca\n"
+
+    with open(f"{GTFS_OUTPUT_PATH}/agency.txt", "w", encoding="utf-8") as f:
+        f.write(agency_txt_content)
+
+
+def main() -> None:
+    print("Creating agency information...")
+    create_agency_content()
+
+
+if __name__ == "__main__":
+    main()
